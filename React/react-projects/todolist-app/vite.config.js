51771aab1e8e038d786e539bfede07be15681403
Vigneshwar <vigneshwar.s1996@gmail.com>
2025-10-18 19:21:25 +0530

    fix: update gh-pages dependency location and correct base path in vite config

--- React/react-projects/todolist-app/vite.config.js
+++ React/react-projects/todolist-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-projects/todolist-app/', // IMPORTANT: repo/folder name
+  base: '/studies/react/react-projects/todolist-app/', // IMPORTANT: repo/folder name
   plugins: [react()],
 })
