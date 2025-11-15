ab66ff8bb701dfbd2a0134b4e73ef09f2f17bcbe
Vigneshwar <vigneshwar.s1996@gmail.com>
2025-11-16 05:10:49 +0530

    testing forms

--- React/react-projects/formik-react-hook-form/src/App.jsx
+++ React/react-projects/formik-react-hook-form/src/App.jsx
@@ -1,51 +1,202 @@
-import React from 'react'
-import {useForm, Controller} from 'react-hook-form'
-import Select from "react-select"
+// RHFForm.jsx
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-export default function App() {
-  const {register, handleSubmit, formState:{errors, isSubmitting, isValid}, control, watch} = useForm({
+export default function RHFForm() {
+  const {register,handleSubmit,control,watch,reset,formState: { errors, isSubmitting, isValid, touchedFields }} = useForm({
+    mode: "all",
     defaultValues: {
-      "email": "test",
-       "gender": { label: "Female", value: "female" }
-    },
-    mode: "all"
-  })
+      email: "",
+      password: "",
+      phone: "",
+      comments: "",
+      gender: "",
+      country: null,            // react-select (object)
+      hobbies: [],              // checkbox group
+      agree: false,             // single checkbox
+      newsletter: "daily",      // radio (string)
+      birthDate: null,          // Date object
+      avatar: null,             // file
+      address: { city: "", zip: "" } // nested object example
+    }
+  });
 
-  const all = watch()
+  const onSubmit = (data) => {
+    // Note: file input (avatar) is a FileList; convert if needed before sending
+    console.log("RHF submit:", data);
+  };
 
-  const onSubmit = data => {
-    console.log(data);
-  }
+  // live watch example
+  const allValues = watch();
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("email", {required: "This field is required", pattern: {value: /\S+@\S+\.\S+/, message: "Invalid format"}})} placeholder='email' />
-        {errors.email && <p>{errors.email.message}</p>} <br />
-        <input {...register("name", {required: "This field is required"})} placeholder='name' />
-        {errors.name && <p>{errors.name.message}</p>} <br />
-        <input type='number' {...register("age",{min: {value: 18, message: "Minimum is 18"}, valueAsNumber: true})} placeholder='age' />
-        {errors.age && <p>{errors.age.message}</p>} <br />
-        <input {...register("phone",{required: "This field is required", minLength: {value: 10, message: "Min is 10 letters"}, maxLength:{value: 10, message: "max is 10"}})} placeholder='phone' />
-        {errors.phone && <p>{errors.phone.message}</p>} <br />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <br />
+      {/* Email */}
+      <label>Email</label>
+      <input
+        {...register("email", {
+          required: "Email required",
+          pattern: { value: /\S+@\S+\.\S+/, message: "Invalid email" }
+        })}
+        placeholder="you@email.com"
+      />
+      {errors.email && <p>{errors.email.message}</p>}
 
-        <Controller name='gender' rules={{required: "Select gender"}} control={control} render={({field, fieldState: {error}}) => (
+      <br />
+      {/* Password */}
+      <label>Password</label>
+      <input
+        type="password"
+        {...register("password", {
+          required: "Password required",
+          minLength: { value: 6, message: "Min 6 chars" }
+        })}
+      />
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <br />
+      {/* Phone (string - NOT valueAsNumber) */}
+      <label>Phone</label>
+      <input
+        {...register("phone", {
+          required: "Phone required",
+          minLength: { value: 10, message: "Must be 10 digits" },
+          maxLength: { value: 10, message: "Must be 10 digits" },
+          pattern: { value: /^[0-9]+$/, message: "Digits only" }
+        })}
+        placeholder="9876543210"
+        inputMode="numeric"
+      />
+      {errors.phone && <p>{errors.phone.message}</p>}
+
+      <br />
+      {/* Comments / textarea */}
+      <label>Comments</label>
+      <textarea {...register("comments")} placeholder="Any notes..." />
+
+      <br />
+      {/* Radio */}
+      <br />
+      <label>Newsletter frequency</label>
+      <label>
+        <input {...register("newsletter")} type="radio" value="daily" />
+        Daily
+      </label>
+      <label>
+        <input {...register("newsletter")} type="radio" value="weekly" />
+        Weekly
+      </label>
+
+      <br />
+      {/* Select (native) */}
+      <label>Gender</label>
+      <select {...register("gender", { required: "Select gender" })}>
+        <option value="">-- select --</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+      </select>
+      {errors.gender && <p>{errors.gender.message}</p>}
+
+      <br />
+      {/* react-select (controlled) */}
+      <label>Country (react-select)</label>
+      <Controller
+        name="country"
+        control={control}
+        rules={{ required: "Choose a country" }}
+        render={({ field, fieldState: { error } }) => (
           <>
-            <Select {...field} options={[{label: "Male", value: "male"}, {label: "Female", value: "female"}]} />
-            {error && <p>{error.message}</p>}<br />
+            <Select
+              {...field}
+              // field.value is either null or the selected option object
+              options={[
+                { label: "India", value: "IN" },
+                { label: "United States", value: "US" },
+                { label: "United Kingdom", value: "UK" }
+              ]}
+              isClearable
+              onChange={(val) => field.onChange(val)}
+            />
+            {error && <p>{error.message}</p>}
           </>
-        )} />
+        )}
+      />
 
-        <button type='submit' disabled={!isValid}>{isSubmitting ? "Submitting....." : "Submit"}</button>
-      </form>
+      <br />
+      {/* Checkbox group -> hobbies */}
+      <br />
+      <label>Hobbies</label>
+      <label>
+        <input type="checkbox" value="reading" {...register("hobbies")} />
+        Reading
+      </label>
+      <label>
+        <input type="checkbox" value="coding" {...register("hobbies")} />
+        Coding  
+      </label>
+      <label>
+        <input type="checkbox" value="sports" {...register("hobbies")} />
+        Sports
+      </label>
 
+      <br />
+      {/* Single required checkbox */}
+      <label>
+        <input {...register("agree", { required: "You must accept terms" })} type="checkbox" />
+        I agree to terms
+      </label>
+      {errors.agree && <p>{errors.agree.message}</p>}
 
-      <h2>form datas</h2>
-      <pre>{JSON.stringify(all, null, 2)}</pre>
-    </div>
-  )
+      <br />
+      {/* DatePicker (react-datepicker via Controller) */}
+      <label>Birth date</label>
+      <Controller
+        name="birthDate"
+        control={control}
+        rules={{ required: "Birth date required" }}
+        render={({ field, fieldState: { error } }) => (
+          <>
+            <DatePicker
+              selected={field.value}
+              onChange={(val) => field.onChange(val)}
+              dateFormat="dd/MM/yyyy"
+              placeholderText="Select a date"
+            />
+            {error && <p>{error.message}</p>}
+          </>
+        )}
+      />
+
+      <br />
+      {/* File input */}
+      <label>Avatar (file)</label>
+      <input
+        type="file"
+        {...register("avatar")}
+        accept="image/*"
+      />
+
+      <br />
+      {/* Nested fields example */}
+      <label>City</label>
+      <input {...register("address.city")} placeholder="City" />
+      <br />
+      <label>ZIP</label>
+      <input {...register("address.zip")} placeholder="ZIP" />
+      <br />
+      {/* Submit + reset */}
+      {/* <button type="submit" disabled={isSubmitting || !isValid}>Submit</button> */}
+      <button type="submit">Submit</button><br />
+      <button type="button" onClick={() => reset()}>Reset</button>
+
+      {/* live preview */}
+      <h4>Live values (watch):</h4>
+      <pre>{JSON.stringify(allValues, null, 2)}</pre>
+    </form>
+  );
 }
-// email
-// name
-// age
-// phone
--- React/react-projects/formik-react-hook-form/src/App2.jsx
+++ React/react-projects/formik-react-hook-form/src/App2.jsx
@@ -1,62 +1,148 @@
-import React from 'react'
-import {Formik, Form, Field, ErrorMessage, useFormikContext} from 'formik'
-import * as Yup from 'yup'
-import Select from 'react-select'
+// FormikForm.jsx
+import React from "react";
+import { Formik, Form, Field, ErrorMessage, useFormikContext } from "formik";
+import Select from "react-select";
+import DatePicker from "react-datepicker";
+import * as Yup from "yup";
+import "react-datepicker/dist/react-datepicker.css";
 
-export default function App2() {
+const schema = Yup.object({
+  email: Yup.string().email("Invalid email").required("Email required"),
+  password: Yup.string().min(6, "Min 6 chars").required("Password required"),
+  phone: Yup.string()
+    .matches(/^[0-9]{10}$/, "Phone must be 10 digits")
+    .required("Phone required"),
+  comments: Yup.string().nullable(),
+  gender: Yup.string().required("Select gender"),
+  country: Yup.object().nullable().required("Choose a country"),
+  hobbies: Yup.array().of(Yup.string()),
+  agree: Yup.boolean().oneOf([true], "You must accept terms"),
+  birthDate: Yup.date().nullable().required("Birth date required"),
+  address: Yup.object({
+    city: Yup.string().nullable(),
+    zip: Yup.string().nullable()
+  })
+});
 
-    const schema = Yup.object({
-        email: Yup.string().matches(/\S+@\S+\.\S+/, "Invalid email format").required("Email is required"),
-        password: Yup.string().min(6,"minimum 6 fields").required("Password is required"),
-        gender: Yup.object().required("Select Gender")
-    })
-    const LivePreview = () => {
-        const { values } = useFormikContext();
-        return <pre>{JSON.stringify(values, null, 2)}</pre>;
-    };
+const LivePreview = () => {
+  const { values } = useFormikContext();
+  return <pre>{JSON.stringify(values, null, 2)}</pre>;
+};
 
+export default function FormikForm() {
   return (
-    <div>
-        
-      <Formik initialValues={{email: "terst", password: "", gender: null}} validationSchema={schema} onSubmit={values => console.log(values)}>
-        <>
-            <LivePreview />
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+        phone: "",
+        comments: "",
+        gender: "",
+        country: null,
+        hobbies: [],
+        agree: false,
+        newsletter: "daily",
+        birthDate: null,
+        address: { city: "", zip: "" }
+      }}
+      validationSchema={schema}
+      onSubmit={(values) => {
+        console.log("Formik submit:", values);
+      }}
+    >
+      {({ values, setFieldValue, setFieldTouched }) => (
+        <Form>
+          <label>Email</label>
+          <Field name="email" placeholder="you@email.com" />
+          <ErrorMessage name="email" component="p" />
 
-            <Form>
-              <Field name="email" placeholder="email" />
-              <ErrorMessage name="email" component="p" />
-              <br />
+          <br />
+          <label>Password</label>
+          <Field name="password" type="password" />
+          <ErrorMessage name="password" component="p" />
 
-              <Field name="password" placeholder="password" />
-              <ErrorMessage name="password" component="p" />
-              <br />
+          <br />
+          <label>Phone</label>
+          <Field name="phone" placeholder="9876543210" />
+          <ErrorMessage name="phone" component="p" />
 
-              <Field name="gender">
-                {({ field, form, meta }) => (
-                  <>
-                    <Select
-                      options={[
-                        { label: "Male", value: "male" },
-                        { label: "Female", value: "female" }
-                      ]}
-                      value={field.value}
-                      onChange={(option) =>
-                        form.setFieldValue(field.name, option)
-                      }
-                      onBlur={() =>
-                        form.setFieldTouched(field.name, true)
-                      }
-                    />
+          <br />
+          <label>Comments</label>
+          <Field name="comments" as="textarea" />
 
-                    {meta.touched && meta.error && <p>{meta.error}</p>}
-                  </>
-                )}
-              </Field>
+          <br />
+          <label>Newsletter</label>
+          <label>
+            <Field name="newsletter" type="radio" value="daily" /> Daily
+          </label>
+          <label>
+            <Field name="newsletter" type="radio" value="weekly" /> Weekly
+          </label>
 
-              <button type="submit">Submit</button>
-            </Form>
-          </>
-      </Formik>
-    </div>
-  )
+          <br />
+          <label>Gender (native select)</label>
+          <Field name="gender" as="select">
+            <option value="">-- select --</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </Field>
+          <ErrorMessage name="gender" component="p" />
+
+          <br />
+          <label>Country (react-select)</label>
+          <Select
+            options={[
+              { label: "India", value: "IN" },
+              { label: "United States", value: "US" }
+            ]}
+            value={values.country}
+            onChange={(option) => setFieldValue("country", option)}
+            onBlur={() => setFieldTouched("country", true)}
+            isClearable
+          />
+          <ErrorMessage name="country" component="p" />
+
+          <br />
+          <label>Hobbies (checkbox group)</label>
+          <label>
+            <Field type="checkbox" name="hobbies" value="reading" /> Reading
+          </label>
+          <label>
+            <Field type="checkbox" name="hobbies" value="coding" /> Coding
+          </label>
+
+          <br />
+          <label>
+            <Field type="checkbox" name="agree" />
+            Agree
+          </label>
+          <ErrorMessage name="agree" component="p" />
+
+          <br />
+          <label>Birth date</label>
+          <DatePicker
+            selected={values.birthDate}
+            onChange={(date) => setFieldValue("birthDate", date)}
+            dateFormat="dd/MM/yyyy"
+            onBlur={() => setFieldTouched("birthDate", true)}
+          />
+          <ErrorMessage name="birthDate" component="p" />
+
+          <br />
+          <label>City</label>
+          <Field name="address.city" />
+          <br />
+          <label>ZIP</label>
+          <Field name="address.zip" />
+          <br />
+          <button type="submit">Submit</button><br />
+          <button type="reset">Reset</button>
+
+          <h4>Live values</h4>
+          <LivePreview />
+        </Form>
+      )}
+    </Formik>
+  );
 }
