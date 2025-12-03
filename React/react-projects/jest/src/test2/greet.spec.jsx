5094f9de699a5cb75fc2beef3f7307569ee3bc64
Vigneshwar <vigneshwar.s1996@gmail.com>
2025-12-04 00:03:56 +0530

    feat: add Greet component and corresponding tests; include Jest tutorial notes

---
+++ React/react-projects/jest/src/test2/greet.spec.jsx
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom'
+import Greet from './Greet'
+import {render, screen} from '@testing-library/react'
+import {userEvent} from '@testing-library/user-event'
+import React from 'react'
+
+test("Testing greet component", () => {
+    render(<Greet />)
+    expect(screen.getByText(/hello/i)).toBeInTheDocument()
+})
