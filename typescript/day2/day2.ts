29bedf1305c14d89ec3142bcdd8ba57dc22bc968
Vigneshwar <vigneshwar.s1996@gmail.com>
2025-12-16 17:57:09 +0530

    Day 2 completed refactor: update TypeScript files and HTML structure for day 2

--- typescript/day2/day2.ts
+++ typescript/day2/day2.ts
@@ -1,7 +1,10 @@
 // day 2: function, narrowing & literals
 //-------------------------------------------------------
+
+import { assert } from "node:console";
+
 // 1) function parametrs and return types
-function sum(a:number, b:number):number{
+function sum1(a:number, b:number):number{
     return a + b;
 }
 // always use type paraments, let ts infer return type unless it is complex
@@ -31,4 +34,108 @@ console.log(greet1());
 // prefer default over optional parameters
 
 //-------------------------------------------------------
-// 3)
+// 3) typeof narrowing
+
+function printValue(value:string|number){
+    if(typeof value === "string"){
+        return value.toLocaleUpperCase()
+    }else{
+        return value * 10;
+    }
+}
+
+console.log(printValue("hellow"))
+console.log(printValue(12))
+
+// in operator (in operator is a js only, just checking how it works here)
+
+type Admin = {
+    person: "admin",
+    permission: string[]
+}
+
+type User = {
+    person: "employee"
+}
+
+function checkPermission(person:Admin | User){
+    if("permission" in person){
+        return person.permission
+    }
+}
+
+console.log(checkPermission({person: "employee"}));
+console.log(checkPermission({person: "admin", permission: ["all access"]}));
+
+//-------------------------------------------------------
+
+// 4) literal types
+
+type buttonVariant = "primary" | "default"
+
+function renderButton(buttonType: buttonVariant){
+    return `<button>${buttonType}</button>`
+}
+
+console.log(renderButton("primary"));
+// console.log(renderButton("blue")); // error
+
+//-------------------------------------------------------
+
+// 5) discriminated unions
+
+type apiResponse = {status: "loading"} | {status: "success", data: string[]} | {status: "error", message: string}
+
+function handleResponse(api: apiResponse){
+    if(api.status === "success"){
+        return api.data
+    }
+}
+
+console.log(handleResponse({status: "success", data: ["test data"]}));
+
+//-------------------------------------------------------
+// 6) never (signals impossible cases)
+
+// so "never" is basically additional checking on top of ts inference
+
+function fail(message: string):never{
+    throw new Error(message)
+}
+
+// console.log(fail("error message"));
+
+//-------------------------------------------------------
+
+// 7) avoid enums, use unions instead
+
+// enum Status {Loading, Succes, Error}
+// type Status = "loading" | "success" | "error"
+
+//-------------------------------------------------------
+// 📝 Day 2 Practice Task (IMPORTANT)
+
+type Status = "loading" | "success" | "Error" | "idle"
+
+function assertNever(value: never):never{
+    throw new Error(value)
+}
+
+function fetchStatus(status: Status) {
+
+    switch(status){
+        case "loading":
+            return "Loading..."
+        case "success":
+            return "Data loaded"
+        case "Error":
+            return "Error"
+        // default:
+        //     return assertNever(status) // this shows idle is not used error
+    }
+}
+
+console.log(fetchStatus("loading"))
+console.log(fetchStatus("success"))
+console.log(fetchStatus("Error"))
+// console.log(fetchStatus("test"))
